4fd02a9611ff03b483e07bc641a1f7168f13469c
Memeslav <122861199+Memeslav@users.noreply.github.com>
2024-11-11 10:16:37 +0300

    Added simple fish to main scene

---
+++ scenes/Fish/fish.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var speed = 400
+var angular_speed = PI
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Hewwo!")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+
+	position += velocity * delta
